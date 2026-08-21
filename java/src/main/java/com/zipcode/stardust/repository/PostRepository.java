002c6ea944c51b096fea49f8b60a6e304ddc3b6e
matthew <matt.gregory89@gmail.com>
2026-08-21 09:13:02 -0400

    user name persists add most recent posts

--- java/src/main/java/com/zipcode/stardust/repository/PostRepository.java
+++ java/src/main/java/com/zipcode/stardust/repository/PostRepository.java
@@ -1,14 +1,22 @@
 package com.zipcode.stardust.repository;
 
-import com.zipcode.stardust.model.Post;
-import com.zipcode.stardust.model.Subforum;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zipcode.stardust.model.Post;
+import com.zipcode.stardust.model.Subforum;
+import com.zipcode.stardust.model.User;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
     List<Post> findBySubforumOrderByPostdateDesc(Subforum subforum);
+
     List<Post> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(
-        String title,
-        String content
-);
+            String title,
+            String content
+    );
+
+    // Get the user's 5 most recent posts
+    List<Post> findTop5ByUserOrderByPostdateDesc(User user);
 }
